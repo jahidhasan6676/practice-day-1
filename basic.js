1280e3520bacff318456a204e240da592adb01ce
jahidhasan6676 <jh18186676@gmail.com>
2024-09-04 11:59:04 +0600

    added the practice day all file

---
+++ basic.js
@@ -0,0 +1,139 @@
+// const money = 100;
+// if(money >= 100){
+//    if (money < 180){
+//     console.log('ami biriyani khabo');
+//    }
+//    if(money > 150){
+//     console.log('ami moglai khabo');
+//    }
+//    if(money === 100){
+//     console.log('ami singara puri khabo')
+//    }
+
+    
+// }
+// else{
+//     console.log('ami rikshaw diye bari chole jabo');
+// }
+
+// const passed = false;
+// if(!passed == false ){
+//     console.log('hello java')
+// }
+// else{
+//     console.log('hello jahid')
+// }
+
+
+// 
+
+// const money = 20;
+// console.log(money % 3)
+
+// const isRainy = true;
+// const umbrella = true;
+// if(isRainy === true){
+//     if(umbrella === false){
+//         console.log('ami baire jabo');
+//     }
+//     if(umbrella === true){
+//         console.log('ami gore thakbo')
+//     }
+// }
+// else{
+//     console.log('ami gumaboo')
+// }
+
+// const goldenGrad = 100;
+// const fail = 40;
+// const bangla = 80;
+// const english = 70;
+// const math = 60;
+// const ict = 90;
+// const economic = 50;
+
+// if(goldenGrad > fail){
+//     if(bangla >= 80 && bangla > 70){
+//         console.log('Grade- A');
+//     }
+//     if(english >= 70 && english > 60){
+//         console.log('Grade- B')
+//     }
+//     if(math >= 60 && math > 50){
+//         console.log('Grade- D')
+//     }
+//     if(ict >= 90 && ict > 80){
+//         console.log('Grade- A+')
+//     }
+//     if(economic >= 50 && economic > 40){
+//         console.log('Grade- C')
+//     }
+// }
+// else{
+//     console.log('Grade- F')
+// }
+
+// const applePrice = 150;
+// const haveMoney = 120;
+// const mangoPrice = 80;
+// const orangePrice = 100;
+
+// if(haveMoney >= 120){
+//     if(applePrice < haveMoney){
+//         console.log('i can buy apple')
+//     }
+
+//     if(mangoPrice <= haveMoney){
+//         console.log('i can buy mango');
+//     }
+//     if(orangePrice < haveMoney){
+//         console.log('i can buy orange')
+//     }
+// }
+
+
+// else{
+//     console.log('rikshaw diye bari chole jabo')
+// }
+
+// const friends = ['jahid', 'hasan', 'kamrul', 'saimon'];
+// result = friends.join('|')
+
+// console.log(result)
+
+
+// const friends = ['jahid', 'hasan', 'kamrul', 'saimon'];
+// out = friends.pop('saimon')
+// console.log(friends)
+// console.log(out)
+
+// var price = 40;
+// price += 20;
+// console.log(price);
+
+// const price = 40;
+// // if(price > 50){
+// //     console.log('moglai khabo')
+// // }
+// // else{
+// //     console.log('jalmuri khabo')
+// // }
+
+//   names = price > 50 ? 'moglai khabo' : 'jalmuri khabo';
+//  console.log(names)
+ 
+
+// let price = 40;
+// price += 10;
+// console.log(price);
+
+// const money = 100;
+// names = money >= 111 ? 'biriyani khabo' :  'vat khabo';
+// console.log(names)
+
+const y = 10;
+const x = 3;
+divide = y / 3;
+console.log( divide);
+total = divide.toFixed(4);
+console.log(5 + typeof parseFloat(total));
